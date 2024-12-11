ba389975ad308267d864abbfce27800c5ba71170
Curious-Goblin <poddarsourabh9939@gmail.com>
2024-12-11 19:01:54 +0530

    Initial Commit

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import db from "@repo/db/client";
+const app = express();
+
+app.post("./hdfcWebhook", async (req, res) => {
+  // TODO: Add zod validation here
+  // check if this request actually came from hdfc and a webhook secret key
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.user_identifier,
+    amount: req.body.amount,
+  };
+  // update balance in db, add txn
+  try {
+    await db.$transaction([
+
+      db.balance.update({
+        where: {
+          userId: paymentInformation.userId,
+        },
+        data: {
+          amount: {
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+
+      db.onRampTransaction.update({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.status(200).json({
+      message: "captured",
+    });
+
+  } 
+  
+  catch (e) {
+    console.log(e);
+    res.status(411).json({
+      message: "Error while processing Webhook",
+    });
+  }
+});
+
+app.listen(3003);
